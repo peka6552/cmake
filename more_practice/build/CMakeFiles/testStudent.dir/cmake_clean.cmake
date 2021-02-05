c8a85c4764a5bfad348b6ce3c1b1119b263fc85c
PekaShu <peka6552@gmail.com>
2021-02-05 12:28:37 +0530

    cmake learning

---
+++ more_practice/build/CMakeFiles/testStudent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testStudent.dir/src/Student.cpp.o"
+  "CMakeFiles/testStudent.dir/src/mainapp.cpp.o"
+  "testStudent"
+  "testStudent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testStudent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
